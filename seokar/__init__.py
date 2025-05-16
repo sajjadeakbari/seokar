844c370889372ab9f218f3f86e725d93a4109c6f
sajjadakbari <sajjadakbari.dv@gmail.com>
2025-05-17 02:40:57 +0330

    Update __init__.py

--- seokar/__init__.py
+++ seokar/__init__.py
@@ -6,6 +6,6 @@ Seokar: A Python library for comprehensive on-page SEO analysis.
 
 __version__ = "0.1.0"
 
-from .analyzer import Seokar
+from .seokar import Seokar, SEOResult, SEOResultLevel 
 
-__all__ = ["Seokar"]
+__all__ = ["Seokar", "SEOResult", "SEOResultLevel", "__version__"]
