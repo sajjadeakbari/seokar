43176448278c94aa75faaf5fb1421ad28c4a8e3c
sajjadakbari <sajjadakbari.dv@gmail.com>
2025-06-06 17:51:03 +0330

    Create __init__.py
    Signed-off-by: sajjadakbari <sajjadakbari.dv@gmail.com>

---
+++ seokar/constants/__init__.py
@@ -0,0 +1,24 @@
+from .core import SeoLimits, StatusCode, RobotsTxt
+from .technical import CoreWebVitals, PageSpeed, TechnicalIssues
+from .content import ContentQuality, ReadabilityFormulas
+from .local import LocalSeo, ContactInfo
+from .schema import SchemaTypes, SchemaProperties, SchemaUrls
+from .security import SecurityHeaders, RecommendedSecurityValues
+
+__all__ = [
+    "SeoLimits",
+    "StatusCode",
+    "RobotsTxt",
+    "CoreWebVitals",
+    "PageSpeed",
+    "TechnicalIssues",
+    "ContentQuality",
+    "ReadabilityFormulas",
+    "LocalSeo",
+    "ContactInfo",
+    "SchemaTypes",
+    "SchemaProperties",
+    "SchemaUrls",
+    "SecurityHeaders",
+    "RecommendedSecurityValues",
+]
